f88a8662fda6940a03722565f06326a21b5f1f60
Sanket Athawale <athawalesanket0@gmail.com>
2024-11-28 23:57:28 +0530

    added database file

---
+++ sql/database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE weeho;
+
+USE weeho;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
