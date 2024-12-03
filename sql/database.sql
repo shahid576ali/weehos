021e8d85a47cb90a6fc65ad4f518c67c4276a7ac
Sanket Athawale <athawalesanket0@gmail.com>
2024-12-03 21:44:17 +0530

    Updated database

--- sql/database.sql
+++ sql/database.sql
@@ -8,3 +8,21 @@ CREATE TABLE users (
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE artists (
+    artist_id INT AUTO_INCREMENT PRIMARY KEY,
+    artistName VARCHAR(50) NOT NULL,
+    genre VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    bio TEXT
+);
+
+CREATE TABLE admins (
+    admin_id INT AUTO_INCREMENT PRIMARY KEY,
+    fullName VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    organization VARCHAR(100),
+    adminKey VARCHAR(50) NOT NULL UNIQUE
+);
